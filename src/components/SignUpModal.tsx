8060ba06aa397cce9492e803843d6407619467f5
Jomar Amomas <jplus2@outlook.com>
2024-08-09 06:58:14 +0800

    Add sign up form validation.

--- src/components/SignUpModal.tsx
+++ src/components/SignUpModal.tsx
@@ -3,9 +3,17 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 import { auth } from "../firebaseConfig";
 import TechStackPulseSmallLogo from "../assets/tsp-logo-small.svg";
+import { signUpSchema } from "../schemas";
 import Modal from "./common/Modal";
 import Input from "./common/Input";
 import PasswordInput from "./common/PasswordInput";
+
+interface IFormErrors {
+  email?: string[];
+  fullname: string[];
+  password: string[];
+  passwordConfirm: string[];
+}
 
 const SignUpModal: FC<{
   isVisible: boolean;
@@ -17,13 +25,29 @@ const SignUpModal: FC<{
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setLoading] = useState(false);
+  const [errors, setErrors] = useState<IFormErrors | null>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
 
-    if (password !== passwordConfirm) {
-      alert("Passwords do not match");
+    const validationResult = signUpSchema.safeParse({
+      email,
+      fullname,
+      password,
+      passwordConfirm,
+    });
+
+    if (!validationResult.success) {
+      /**
+       * Format will be:
+       *
+       * {
+       *   email:    [ 'Invalid Email' ],
+       *   password: [ 'Password error 1', 'Password error 2' ]
+       * }
+       */
+      setErrors(validationResult.error.flatten().fieldErrors as IFormErrors);
       setLoading(false);
       return;
     }
@@ -55,6 +79,7 @@ const SignUpModal: FC<{
     setFullname("");
     setPassword("");
     setPasswordConfirm("");
+    setErrors(null);
     toggleModal();
   };
 
@@ -81,22 +106,26 @@ const SignUpModal: FC<{
               title="Email address"
               placeholder="email@email.com"
               value={email}
+              error={errors?.email}
               onChange={(e) => setEmail(e.target.value)}
             />
             <Input
               title="Fullname"
               placeholder="John Doe"
               value={fullname}
+              error={errors?.fullname}
               onChange={(e) => setFullname(e.target.value)}
             />
             <PasswordInput
               title="Password"
               value={password}
+              error={errors?.password}
               onChange={(e) => setPassword(e.target.value)}
             />
             <PasswordInput
               title="Re-enter password"
               value={passwordConfirm}
+              error={errors?.passwordConfirm}
               onChange={(e) => setPasswordConfirm(e.target.value)}
             />
           </div>
