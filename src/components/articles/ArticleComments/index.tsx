c275835a3237798ae6507817c69dda619be64438
Jomar Amomas <jplus2@outlook.com>
2024-08-22 06:55:01 +0800

    Separate article comment form into its own component.

--- src/components/articles/ArticleComments/index.tsx
+++ src/components/articles/ArticleComments/index.tsx
@@ -1,8 +1,9 @@
 import { useImperativeHandle, forwardRef, useState, useEffect } from "react";
 import { CaretDown, XCircle } from "@phosphor-icons/react";
-import { Drawer, Alert, TextField } from "@mui/material";
+import { Drawer, Alert } from "@mui/material";
 
 import { useFetchArticleComments } from "../../../hooks";
+import ArticleCommentForm from "./ArticleCommentForm";
 import ArticleComment, { IArticleComment } from "./ArticleComment";
 import ArticleCommentsSkeleton from "./ArticleCommentsSkeleton";
 
@@ -50,36 +51,7 @@ const PostComments = forwardRef<any, {}>((_, ref) => {
         </div>
 
         {/* Text Area for posting comment */}
-        <div className="w-full shadow-[0_4px_12px_0px_rgba(0,0,0,0.1)] rounded-md p-3 mt-7">
-          <div className="w-full flex items-center">
-            <img
-              className="w-[40px] h-[40px] rounded-full"
-              src="https://s3-alpha-sig.figma.com/img/11db/cb98/2f9ba115c7d5cc790cc48a457815fb67?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bmyQ8PD~U7XIpJu7gryBKa8ILmsPaBoSoA39AyIGYCYrkXbPCsMe9w6exnVzITWgDx9fEMc9thTFYh~ydCNgbSxXnrCl~mkKrKA4~VZyBNXygKDml7~wQF0bXI88vLTwps854CfFk47N5lLwZXB2jmLaMtxt6v5Gn9IMjEkz2l--VUn2qdQ6~woetX87qnA4xCK9xZhIZsPymr4oOScWq-yXhRdBUR3~mRghY7VikRQxHc7lR89mD~oMG-dd9jNGmTZ~8rihFeAhcNgATsj8CcGPia95mUHOCfo1r6YH5bCReRXKc4inh53dQ00xa~s8HESwKlsYr8RcAMuh0fVZ9Q__"
-            />
-            <p className="ml-2 font-semibold text-[16px] text-neutral-900">Erika Albright</p>
-          </div>
-          <TextField
-            autoFocus={false}
-            multiline={true}
-            className="border-none outline-none focus:outline-none w-full"
-            placeholder="What are your thoughts"
-            sx={{
-              "& fieldset": { border: "none" },
-            }}
-            rows={3}
-          ></TextField>
-          <div className="flex justify-end py-2">
-            <button disabled={fetching} className={`w-20 rounded-lg font-normal text-[14px] align-middle ${fetching ? "text-zinc-500" : "text-[#FF2E3D]"}`}>
-              Cancel
-            </button>
-            <button
-              disabled={fetching}
-              className={`ml-2 w-20 rounded-[48px] ${fetching ? "bg-zinc-500" : "bg-[#FF2E3D]"} font-normal text-[14px] text-[#FFFFFF] py-2 px-3`}
-            >
-              Respond
-            </button>
-          </div>
-        </div>
+        <ArticleCommentForm disabled={fetching} />
 
         {/* Comments section */}
         <div className="flex mt-7 align-middle items-center">
